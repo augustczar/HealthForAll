9834b7c80fc24246331a23db8f579d938573cfe7
augustczar <augustczarramos@gmail.com>
2024-07-06 22:11:14 -0300

    Initialization Classes

---
+++ src/main/java/com/augustczar/healthforall/HealthForAllApplication.java
@@ -0,0 +1,12 @@
+package com.augustczar.healthforall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthForAllApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HealthForAllApplication.class, args);
+	}
+}
---
+++ src/test/java/com/augustcar/healthforall/HealthForAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustcar.healthforall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthForAllApplicationTests {
+
+	  @Test
+	  void contextLoads() {
+	  }
+
+}
